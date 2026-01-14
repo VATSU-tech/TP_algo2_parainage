88ef8389e781a2cadd8c2da9b980d971cb6e56d8
VATSU_04 <vatsumalambo@gmail.com>
2026-01-14 20:30:29 +0200

    refactor: Use semantic `main` tag for login page and center body content.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,9 @@
 export default function Login() {
     return (
-        <div className="flex flex-col gap-4 items-center">
+        <main className="flex flex-col gap-4">
             <input type="text" placeholder="Username" className="input" />
             <input type="password" placeholder="Password" className="input" />
             <button className="btn">Login</button>
-        </div>
+        </main>
     );
 }
