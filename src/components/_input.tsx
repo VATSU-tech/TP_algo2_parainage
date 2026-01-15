bf482a2704cc80f1d9dc104996c372fd10f3f1b5
VATSU_04 <vatsumalambo@gmail.com>
2026-01-15 06:50:35 +0200

    feat: Add `required` prop to the `Input` component and apply it to login form fields, changing the login page root element to `form`.

--- src/components/_input.tsx
+++ src/components/_input.tsx
@@ -1,8 +1,8 @@
-export default function Input({ placeholder, type, icon }: { placeholder: string, type: string, icon?: string }) {
+export default function Input({ placeholder, type, icon ,required}: { placeholder: string, type: string, icon?: string ,required?: boolean}) {
     return (
         <div className="flex items-center gap-2">
             <i className={`fa-solid fa-${(icon || "user")}`}></i>
-            <input type={type} placeholder={placeholder} className="input outline-none focus:border-blue-500 w-full" />
+            <input type={type} placeholder={placeholder} required={required} className="input outline-none focus:border-blue-500 w-full" />
         </div>
     );
 }
