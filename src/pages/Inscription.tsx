bc819461700dfc72c5a75751325a7771141851a2
VATSU_04 <vatsumalambo@gmail.com>
2026-01-15 12:18:14 +0200

    refactor: Update page background and border colors, button text color, and replace `a` tags with `NavLink` for improved navigation.

--- src/pages/Inscription.tsx
+++ src/pages/Inscription.tsx
@@ -1,10 +1,11 @@
+import { NavLink } from "react-router-dom";
 import Button1 from "../components/Button1";
 import Input from "../components/_input"; 
 
 export default function Inscription() {
     return (
         
-        <main className="flex flex-col bg-gray-600 p-4 border-gray-500 border-2  rounded-lg gap-4 w-[400px] mx-auto">
+        <main className="flex flex-col bg-gray-100 p-4 border-gray-200 border-2  rounded-lg gap-4 w-[400px] mx-auto">
             <h1 className="text-4xl font-bold">Inscription</h1>
             <Input placeholder="Nom" type="text" icon="user"/>
             <Input placeholder="Prenom" type="text" icon="user"/>
@@ -12,9 +13,11 @@ export default function Inscription() {
             <Input placeholder="Nom d'utilisateur" type="text" icon="user"/>
             <Input placeholder="Mot de passe" type="password" icon="lock"/>
             <Input placeholder="Confirmer le mot de passe" type="password" icon="lock"/>
-            <Button1 text="S'inscrire" page="/" />
+            <NavLink to="/login" className="w-full flex justify-center">
+                <Button1 text="S'inscrire" page="/" />
+            </NavLink>
             <div className="flex gap-4 justify-between">
-                <p>Vous avez un compte <a href="/login" className="text-center text-blue-500 hover:underline">se connecter</a> </p>
+                <p>Vous avez un compte <NavLink to="/login" className="text-center hover:text-blue-500 hover:underline">se connecter</NavLink> </p>
             </div>
         </main>
     );
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,14 +3,14 @@ import Input from "../components/_input";
 
 export default function Login() {
     return (
-        <form className="flex flex-col justify-center  bg-gray-600 p-4 border-gray-500 border-2 rounded-lg gap-4 w-[400px] mx-auto">
+        <form className="flex flex-col justify-center  bg-gray-100 p-4 border-gray-200 border-2 rounded-lg gap-4 w-[400px] mx-auto">
             <h1 className="text-4xl font-bold">Connexion</h1>
             <Input placeholder="Username" required icon="user" type="text"/>
             <Input placeholder="Password" required icon="lock" type="password"/>
             <Button1 text="Se connecter" page="/" />
             <div className="flex gap-4 justify-between">
-                <a href="/inscription" className="text-center text-blue-500 hover:underline">S'inscrire</a>
-                <a href="/mot-de-passe-oublie" className="text-center text-blue-500 hover:underline">Mot de passe oublié ?</a>
+                <a href="/inscription" className="text-center hover:text-blue-500 hover:underline">S'inscrire</a>
+                <a href="/mot-de-passe-oublie" className="text-center hover:text-blue-500 hover:underline">Mot de passe oublié ?</a>
             </div>
         </form>
     );
