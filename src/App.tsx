2d8c4f2576afe4d46b5d37fd528bdc338a7d6de5
VATSU_04 <vatsumalambo@gmail.com>
2026-02-20 12:49:14 +0200

    Ajout du script de déploiement et mise à jour de la configuration

--- src/App.tsx
+++ src/App.tsx
@@ -474,6 +474,7 @@ export default function App() {
 
       {/* Pied de page */}
       <Footer />
+      <img src="https://raw.githubusercontent.com/VATSU-tech/wasomi_site_web/refs/heads/main/src/assets/image_hero.jpeg?token=GHSAT0AAAAAADPTYXCLO5J7T27TTXCO67DO2MUV4MA" alt="photo" />
     </div>
   );
 }
